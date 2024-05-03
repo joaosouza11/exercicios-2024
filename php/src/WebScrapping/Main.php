7af375dff24bebcbf6c5fe2f7651fb5143de81f6
João Souza <joaovitorpsouza11@gmail.com>
2024-05-03 18:19:42 -0300

    Creating the header of the spreadsheet

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -24,6 +24,53 @@ class Main {
     $writer = new Writer();
     # Path where the spreadsheet will be saved
     $writer->openToFile(__DIR__ . '/../../assets/papersData.xlsx');
+
+    # Spreadheet header, with a max num of authors in a paper = 20
+    $header = [
+      Cell::fromValue('ID'),
+      Cell::fromValue('Title'),
+      Cell::fromValue('Type'),
+      Cell::fromValue('Author 1'),
+      Cell::fromValue('Author 1 Institution'),
+      Cell::fromValue('Author 2'),
+      Cell::fromValue('Author 2 Institution'),
+      Cell::fromValue('Author 3'),
+      Cell::fromValue('Author 3 Institution'),
+      Cell::fromValue('Author 4'),
+      Cell::fromValue('Author 4 Institution'),
+      Cell::fromValue('Author 5'),
+      Cell::fromValue('Author 5 Institution'),
+      Cell::fromValue('Author 6'),
+      Cell::fromValue('Author 6 Institution'),
+      Cell::fromValue('Author 7'),
+      Cell::fromValue('Author 7 Institution'),
+      Cell::fromValue('Author 8'),
+      Cell::fromValue('Author 8 Institution'),
+      Cell::fromValue('Author 9'),
+      Cell::fromValue('Author 9 Institution'),
+      Cell::fromValue('Author 10'),
+      Cell::fromValue('Author 10 Institution'),
+      Cell::fromValue('Author 11'),
+      Cell::fromValue('Author 11 Institution'),
+      Cell::fromValue('Author 12'),
+      Cell::fromValue('Author 12 Institution'),
+      Cell::fromValue('Author 13'),
+      Cell::fromValue('Author 13 Institution'),
+      Cell::fromValue('Author 14'),
+      Cell::fromValue('Author 14 Institution'),
+      Cell::fromValue('Author 15'),
+      Cell::fromValue('Author 15 Institution'),
+      Cell::fromValue('Author 16'),
+      Cell::fromValue('Author 16 Institution'),
+      Cell::fromValue('Author 17'),
+      Cell::fromValue('Author 17 Institution'),
+      Cell::fromValue('Author 18'),
+      Cell::fromValue('Author 18 Institution'),
+      Cell::fromValue('Author 19'),
+      Cell::fromValue('Author 19 Institution'),
+      Cell::fromValue('Author 20'),
+      Cell::fromValue('Author 20 Institution'),
+    ];
   }
 
 }
